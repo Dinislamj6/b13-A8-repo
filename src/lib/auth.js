e8424d3ac1a3f52951d9a7da6c64b8a115d889f1
dinislamj6 <dinislamj6@gmail.com>
2026-05-04 21:38:23 +0600

    added next cokige

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,6 +2,7 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { nextCookies } from "better-auth/next-js";
 
 const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db("online-learning-platform");
@@ -10,6 +11,7 @@ export const auth = betterAuth({
   database: mongodbAdapter(db, {
     client
   }),
+  plugins: [nextCookies()] ,
   emailAndPassword: { 
     enabled: true, 
     autoSignIn: true,
