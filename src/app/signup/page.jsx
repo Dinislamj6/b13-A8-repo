0396683c26e7d8eebe010414dd63bd5403e81824
dinislamj6 <dinislamj6@gmail.com>
2026-05-04 21:13:40 +0600

    Added register page some function

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -19,25 +19,34 @@ const SignUpPage = () => {
             email: email, // required
             password: password, // required
             image: photo,
+            fetchOptions: {
+                onSuccess: () => {
+                    toast.success("singUp successful!🎉")
+                    router.push("/")
+                },
+                onError: () => {
+                    toast.error("signUp failed🚫please try again");
+                }
+            }
         })
-         if (error) {
-            toast.error("signUp failed🚫please try again");
-            return;
-        }
-        if(!error){
-            router.push("/")
-           
-        }
-        if(data){
-            toast.success("singUp successful!🎉")
-        }
-        
+        //  if (error) {
+
+        //     return;
+        // }
+        // if(!error){
+
+
+        // }
+        // if (data) {
+
+        // }
+
 
     }
     return (
         <div className='container mx-auto min-h-[80vh] flex justify-center items-center bg-slate-100'>
             <div className='p-4 rounded-xl bg-white my-5'>
-                 <h2 className='font-bold text-2xl text-blue-500 text-center'>Welcome</h2>
+                <h2 className='font-bold text-2xl text-blue-500 text-center'>Welcome</h2>
                 <h2 className='font-bold text-xl text-center mb-4'>Register your account</h2>
                 <form className='space-y-2' onSubmit={handleSubmit(handelSignUp)}>
                     <fieldset className="fieldset">
@@ -64,7 +73,7 @@ const SignUpPage = () => {
 
                     <button className="btn w-full bg-slate-800 text-white">SignUp</button>
                 </form>
-                  <p className='mt-4'>Already have an account? <Link className='text-blue-500' href={"/signin"}>Login</Link></p>
+                <p className='mt-4'>Already have an account? <Link className='text-blue-500' href={"/signin"}>Login</Link></p>
             </div>
         </div>
     );
